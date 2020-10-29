c5faf0c19085c3405df87a1763916b4d67b1c12a
Jacob Pjetursson <jacob@pjetursson.com>
2020-10-29 14:17:38 +0100

    increased gif quality slightly

--- src/ch/idsia/scenarios/Play.java
+++ src/ch/idsia/scenarios/Play.java
@@ -87,7 +87,7 @@ public final class Play {
 			final BasicTask basicTask = new BasicTask(marioAIOptions);
 			GlobalOptions.changeScale2x();
 			marioAIOptions.setVisualization(true);
-			marioAIOptions.setLevelDifficulty(15);
+			marioAIOptions.setLevelDifficulty(2);
 			if (!enemies) {
 				marioAIOptions.setEnemies("off");
 			}
